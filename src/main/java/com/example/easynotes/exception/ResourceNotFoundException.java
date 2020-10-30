934771b734fd9b1ea59946616454294d409e281f
Mande Hamed <mandson1er@gmail.com>
2020-10-30 16:34:29 +0000

    SPRING BOOT utilisation du SWAGGER , CRUD POSQL

---
+++ src/main/java/com/example/easynotes/exception/ResourceNotFoundException.java
@@ -0,0 +1,40 @@
+package com.example.easynotes.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class ResourceNotFoundException extends RuntimeException{
+
+	
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 9153386366344360716L;
+	private String resourceName;
+	    private String fieldName;
+	    private Object fieldValue;
+	    
+	    
+	    
+	    public ResourceNotFoundException( String resourceName, String fieldName, Object fieldValue) {
+	        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+	        this.resourceName = resourceName;
+	        this.fieldName = fieldName;
+	        this.fieldValue = fieldValue;
+	    }
+	    
+	    
+	    public String getResourceName() {
+	        return resourceName;
+	    }
+
+	    public String getFieldName() {
+	        return fieldName;
+	    }
+
+	    public Object getFieldValue() {
+	        return fieldValue;
+	    }
+	    
+	    
+}
---
+++ src/main/java/com/example/easynotes/model/Note.java
@@ -0,0 +1,104 @@
+package com.example.easynotes.model;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class)
+
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, 
+        allowGetters = true)
+
+public class Note implements Serializable {
+	/**
+	 * 
+	 */
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	private static final long serialVersionUID = 8643613715571345133L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    private String title;
+
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+    
+    
+    
+    
+
+	public Note() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Note [id=" + id + ", title=" + title + ", content=" + content + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+    
+    // Getters and Setters ... (Omitted for brevity)
+
+	
+}
